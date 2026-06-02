be5cf834867f19bd4e881a365d0ae77e2d470a88
sparky3387 <sparky3387@users.noreply.github.com>
2026-06-02 13:16:09 +1000

    Replace Bazarr with Unpackerr service in flake.nix

--- flake.nix
+++ flake.nix
@@ -1,60 +1,29 @@
 {
-  description = "minimalbase-ng + bazarr service";
-
+  description = "minimalbase-ng + unpackerr service";
   inputs = {
     nixpkgs.follows = "minimalbase/nixpkgs";
     minimalbase.url = "github:nonrootdocker/minimalbase-ng";
-    bazarr-src = {
-      url = "https://github.com/morpheus65535/bazarr/releases/latest/download/bazarr.zip";
+    unpackerr-src = {
+      url = "github:unpackerr/unpackerr";
       flake = false;
     };
   };
-
-  outputs = { self, nixpkgs, minimalbase, bazarr-src }:
+  outputs = { self, nixpkgs, minimalbase, unpackerr-src }:
   let
     system = "x86_64-linux";
     pkgs = import nixpkgs {
       inherit system;
-      config = {
-        allowUnfree = true;
-      };
+      config.allowUnfree = true;
     };
 
     # ----------------------------
-    # Bazarr Python environment
+    # Unpackerr package
     # ----------------------------
-    bazarrPython = pkgs.python3.withPackages (ps: [
-      ps.gevent
-      ps.gevent-websocket
-      ps.cryptography
-      ps.pyopenssl
-      ps.lxml
-      ps.psutil
-      ps.numpy
-      ps.pillow
-      ps.pysubs2
-      ps.webrtcvad
-      ps.requests
-      ps.apscheduler
-      ps.beautifulsoup4
-      ps.setuptools
-    ]);
-
-    bazarr = pkgs.stdenv.mkDerivation {
-      pname = "bazarr";
-      version = "latest";
-      src = bazarr-src;
-
-      buildInputs = [ bazarrPython ];
-
-      # Skip unpacking because Nix automatically extracts the zip archive when fetching
-      dontUnpack = true;
-
-      # Copy the pre-extracted files directly from the Nix store source
-      installPhase = ''
-        mkdir -p $out/app/bazarr
-        cp -r $src/. $out/app/bazarr/
-      '';
+    unpackerr = pkgs.buildGoModule {
+      pname = "unpackerr";
+      version = "1.0.4";
+      src = unpackerr-src;
+      vendorHash = pkgs.lib.fakeHash;
     };
 
     # ----------------------------
@@ -62,59 +31,45 @@
     # ----------------------------
     passwdFile = pkgs.writeTextDir "etc/passwd" ''
       root:x:0:0:root:/root:/bin/sh
-      bazarr:x:1000:1000:bazarr:/data:/bin/sh
+      unpackerr:x:1000:1000:unpackerr:/data:/bin/sh
     '';
 
     # ----------------------------
-    # ABI generator (Points directly to Nix Store)
+    # ABI descriptor for container-init
     # ----------------------------
-    bazAbi = pkgs.writeTextFile {
-      name = "bazarr-abi.json";
+    unpackerrAbi = pkgs.writeTextFile {
+      name = "unpackerr-abi.json";
       text = builtins.toJSON {
         version = 2;
         process = {
-          # Point directly to the secure, immutable Nix store Python binary:
-          exec = "${bazarrPython}/bin/python"; 
-          args = [
-            "/app/bazarr/bazarr.py"
-            "--no-update"
-            "--config"
-            "/data/"
-          ];
+          exec = "${unpackerr}/bin/unpackerr";
+          args = [ "-c" "/data/unpackerr.conf" ];
         };
       };
-      destination = "/app/main"; 
+      destination = "/app/main";
     };
 
   in {
     packages.${system} = {
-      default = self.packages.${system}.bazarr-image;
-      bazarr-image = pkgs.dockerTools.buildImage {
+      default = self.packages.${system}.unpackerr-image;
+      unpackerr-image = pkgs.dockerTools.buildImage {
         name = "minimalbase-ng";
         tag = "latest";
         fromImage = minimalbase.packages.${system}.base-image;
-
         copyToRoot = pkgs.buildEnv {
           name = "root";
           paths = [
             pkgs.coreutils
             pkgs.tzdata
             pkgs.cacert
-            pkgs.ffmpeg-headless
-            pkgs.unrar
-            pkgs.p7zip
-
-            bazarr
-            bazAbi
+            unpackerr
+            unpackerrAbi
             passwdFile
           ];
         };
-
         config = {
           Entrypoint = [ "${minimalbase.packages.${system}.container-init}/bin/container-init" ];
-
           User = "1000:1000";
-
           Env = [
             "PATH=/bin"
             "TZ=UTC"
