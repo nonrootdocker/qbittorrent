c0a8a9bebb53e894fbed06e4a126b050573f14e1
sparky3387 <sparky3387@users.noreply.github.com>
2026-06-02 12:15:33 +1000

    Replace Sonarr with Lidarr in flake.nix

--- flake.nix
+++ flake.nix
@@ -1,14 +1,14 @@
 {
-  description = "minimalbase-ng + sonarr service";
+  description = "minimalbase-ng + lidarr service";
   inputs = {
     nixpkgs.follows = "minimalbase/nixpkgs";
     minimalbase.url = "github:nonrootdocker/minimalbase-ng";
-    sonarr-src = {
-      url = "https://services.sonarr.tv/v1/download/main/latest?version=4&os=linux&arch=x64";
+    lidarr-src = {
+      url = "https://lidarr.servarr.com/v1/update/master/updatefile?os=linux&runtime=netcore&arch=x64";
       flake = false;
     };
   };
-  outputs = { self, nixpkgs, minimalbase, sonarr-src }:
+  outputs = { self, nixpkgs, minimalbase, lidarr-src }:
   let
     system = "x86_64-linux";
     pkgs = import nixpkgs {
@@ -20,12 +20,12 @@
     opensslLib = pkgs.openssl.out;
     sqliteLib = pkgs.sqlite.out;
     # ----------------------------
-    # Sonarr package
+    # Lidarr package
     # ----------------------------
-    sonarr = pkgs.stdenv.mkDerivation {
-      pname = "sonarr";
+    lidarr = pkgs.stdenv.mkDerivation {
+      pname = "lidarr";
       version = "latest";
-      src = sonarr-src;
+      src = lidarr-src;
       nativeBuildInputs = [
         pkgs.autoPatchelfHook
       ];
@@ -34,10 +34,10 @@
         pkgs.curl
         pkgs.sqlite
         opensslLib
-        sqliteLib
         pkgs.zlib
         pkgs.lttng-ust_2_12
         pkgs.stdenv.cc.cc.lib
+        pkgs.libmediainfo
       ];
       unpackPhase = ''
         tar -xzf $src
@@ -52,17 +52,17 @@
     # ----------------------------
     passwdFile = pkgs.writeTextDir "etc/passwd" ''
       root:x:0:0:root:/root:/bin/sh
-      sonarr:x:1000:1000:sonarr:/data:/bin/sh
+      lidarr:x:1000:1000:lidarr:/data:/bin/sh
     '';
     # ----------------------------
     # ABI generator (Points directly to Nix Store)
     # ----------------------------
-    sonarrAbi = pkgs.writeTextFile {
-      name = "sonarr-abi.json";
+    lidarrAbi = pkgs.writeTextFile {
+      name = "lidarr-abi.json";
       text = builtins.toJSON {
         version = 2;
         process = {
-          exec = "${sonarr}/app/Sonarr/Sonarr";
+          exec = "${lidarr}/app/Lidarr/Lidarr";
           args = [
             "-nobrowser"
             "-data=/data"
@@ -73,8 +73,8 @@
     };
   in {
     packages.${system} = {
-      default = self.packages.${system}.sonarr-image;
-      sonarr-image = pkgs.dockerTools.buildImage {
+      default = self.packages.${system}.lidarr-image;
+      lidarr-image = pkgs.dockerTools.buildImage {
         name = "minimalbase-ng";
         tag = "latest";
         fromImage = minimalbase.packages.${system}.base-image;
@@ -84,8 +84,10 @@
             pkgs.coreutils
             pkgs.tzdata
             pkgs.cacert
-            sonarr
-            sonarrAbi
+            pkgs.chromaprint
+            pkgs.mediainfo
+            lidarr
+            lidarrAbi
             passwdFile
           ];
         };
@@ -96,7 +98,7 @@
             "PATH=/bin"
             "TZ=UTC"
             "LANG=en_US.UTF-8"
-            "LD_LIBRARY_PATH=${pkgs.icu}/lib:${opensslLib}/lib:${pkgs.zlib}/lib:${pkgs.lttng-ust_2_12}/lib:${sqliteLib}/lib"
+            "LD_LIBRARY_PATH=${pkgs.icu}/lib:${opensslLib}/lib:${pkgs.zlib}/lib:${pkgs.lttng-ust_2_12}/lib:${sqliteLib}/lib:${pkgs.libmediainfo}/lib"
           ];
         };
       };
